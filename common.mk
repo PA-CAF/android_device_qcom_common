2f362981f4792d52735bac8e610199e23589a09b
Alex Naidis <alex.naidis@linux.com>
2016-08-03 22:45:30 +0200

    common: Implement a method to directly hook into product makefile
    * add common.mk which gets included from product makefiles
    * move things which don't belong to Boardconfig there
    
    Change-Id: Ie6d3ceccf55578f89f7f18673fa1a78af64abbd0
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- common.mk
+++ common.mk
@@ -1,49 +1,21 @@
-ifneq ($(TARGET_USES_AOSP),true)
 TARGET_USES_QCOM_BSP := true
-endif
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_USES_AOSP := false
 TARGET_HAS_QC_KERNEL_SOURCE := true
 TARGET_USES_QCOM_MM_AUDIO := true
-
-BOARD_USES_ADRENO := true
 
 # include additional build utilities
 include device/qcom/common/utils.mk
 
-ifneq ($(HOST_OS),linux)
-ifneq ($(sdclang_already_warned),true)
-$(warning **********************************************)
-$(warning * SDCLANG is not supported on non-linux hosts.)
-$(warning **********************************************)
-sdclang_already_warned := true
-endif
-else
-# include definitions for SDCLANG
-include device/qcom/common/sdclang/sdclang.mk
-endif
-
-# Qualcomm specific packages
-PRODUCT_PACKAGES += SnapdragonCamera
-
-TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
-
-# SECCOMP Extension
-BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
+ANDROID_COMPILE_WITH_JACK := false
 
 # Advanced DPM
 ifeq ($(TARGET_WANTS_EXTENDED_DPM_PLATFORM),true)
+PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += tcmclient
 PRODUCT_BOOT_JARS += com.qti.dpmframework
 PRODUCT_BOOT_JARS += dpmapi
 PRODUCT_BOOT_JARS += com.qti.location.sdk
-endif
-
-# Block Dash by default
-TARGET_DISABLE_DASH ?= true
-
-# Dash extension
-ifeq ($(TARGET_DISABLE_DASH),false)
-PRODUCT_BOOT_JARS += qcmediaplayer
 endif
 
