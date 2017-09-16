dec1c515d0eb23a513cc5dfa1c80ad52e18c0691
Seraph08 <d-vandenburgh@hvcc.edu>
2017-09-16 18:53:36 -0400

    Use CAF's dtbtool

--- dtbtool/Android.mk
+++ dtbtool/Android.mk
@@ -1,4 +1,5 @@
-ifeq ($(BOARD_KERNEL_SEPARATED_DT),true)
+ifeq ($(TARGET_CUSTOM_DTBTOOL),)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -9,9 +10,9 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS += \
 	-Wall
 
-## Hybrid v1/v2 dtbTool. Use a different name to avoid conflicts with copies in device repos
-LOCAL_MODULE := dtbToolCM
+LOCAL_MODULE := dtbTool
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_EXECUTABLE)
+
 endif
